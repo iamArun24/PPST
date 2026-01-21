2971545fbc0ff172a35c37176d95e8a8420b73af
Arun Kumar Singh <arunkumarsingh_250344@aitpune.edu.in>
2026-01-21 11:30:02 +0530

    Changes

--- PPST/project_1.cpp
+++ PPST/project_1.cpp
@@ -1,3 +1,4 @@
+// Add, Subtraction, Multiplication, Division, Remainder;
 #include <iostream> 
 #include <cmath>
 using namespace std;
@@ -8,15 +9,12 @@ int main(){
     cout<< "Enter the number a;";
     cin>>a;
     cout<< "Enter the number b;";
-    cin>>b;
-
-// Add, Substraction, Multiplication, Division, Remainder;
-    
-    cout<<a+b<<endl;
-    cout<<a-b<<endl;
-    cout<<a*b<<endl;
-    cout<<a/b<<endl;
-    cout<<fmod(a,b)<<endl;
+    cin>>b;    
+    cout<<"Addition;"<<a+b<<endl;
+    cout<<"Subtraction;"<<a-b<<endl;
+    cout<<"Multiplication;"<<a*b<<endl;
+    cout<<"Division;"<<a/b<<endl;
+    cout<<"Remainder;"<<fmod(a,b)<<endl;
 
 return 0;
 }
